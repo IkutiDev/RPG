d13cfb3564b2ebcc33ce0e354ac4eb7de1b4f154
Ikuti <m.j.olkiewicz@gmail.com>
2020-10-04 14:07:19 +0200

    Added character model, animations to work with movement, movement and camera tweaks

--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,22 +9,50 @@ using UnityEngine.InputSystem;
 public class Mover : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    private bool holdingMouseButtonForMovement;
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
-    public void MoveToCursor(InputAction.CallbackContext context)
+    private void Update()
+    {
+        if (holdingMouseButtonForMovement)
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed",speed);
+    }
+
+    public void MovementInput(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
-            if (hasHit)
-            {
-                navMeshAgent.destination = hit.point;
-            }
+            holdingMouseButtonForMovement = true;
+        }
+        if (context.canceled)
+        {
+            holdingMouseButtonForMovement = false;
+        }
+    }
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            navMeshAgent.destination = hit.point;
         }
     }
 }
