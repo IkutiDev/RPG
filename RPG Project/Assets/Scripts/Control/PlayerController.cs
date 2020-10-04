8fca85615ad3a0837881a27c0e0869dbe22b0dc4
Ikuti <m.j.olkiewicz@gmail.com>
2020-10-04 23:23:44 +0200

    Added in Placeholder combat and action priority

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -3,47 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using RPG.Movement;
+using RPG.Combat;
+using System;
+
 namespace RPG.Control
 {
-    [RequireComponent(typeof(Mover))]
+    [RequireComponent(typeof(Mover))][RequireComponent(typeof(Fighter))]
     public class PlayerController : MonoBehaviour
     {
-        private bool holdingMouseButtonForMovement;
+        private bool movementHeld;
+        private bool attackPressed;
         private Mover mover;
+        private Fighter fighter;
         // Start is called before the first frame update
         void Start()
         {
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (holdingMouseButtonForMovement)
+            if (InteractWithCombat()) { return; }
+            if(InteractWithMovement()) { return; }
+            Debug.Log("Nothing to do");
+        }
+
+        private bool InteractWithCombat()
+        {
+           RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(var hit in hits)
             {
-                MoveToCursor();
+                var target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) { continue; }
+                if (Mouse.current.rightButton.wasPressedThisFrame)
+                {
+                    fighter.Attack(target);
+                }
+                return true;
             }
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (hasHit)
+            {
+                if (movementHeld)
+                {
+                    mover.MoveTo(hit.point);
+                }
+                return true;
+            }
+            return false;
         }
         public void MovementInput(InputAction.CallbackContext context)
         {
             if (context.performed)
             {
-                holdingMouseButtonForMovement = true;
+                movementHeld = true;
             }
             if (context.canceled)
             {
-                holdingMouseButtonForMovement = false;
+                movementHeld = false;
             }
         }
-        private void MoveToCursor()
+        //public void AttackInput(InputAction.CallbackContext context)
+        //{
+        //    if (context.performed)
+        //    {
+        //        attackPressed = true;
+        //    }
+        //}
+
+        private static Ray GetMouseRay()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
-            if (hasHit)
-            {
-                mover.MoveTo(hit.point);
-            }
+            return Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         }
     }
 }
