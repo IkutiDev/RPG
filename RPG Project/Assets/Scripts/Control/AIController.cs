645fa9b655eb488423dff524e28737709b82f364
Ikuti <m.j.olkiewicz@gmail.com>
2020-10-08 00:57:44 +0200

    Added patrol path + suspiction behaviour for enemies

--- RPG Project/Assets/Scripts/Control/AIController.cs
+++ RPG Project/Assets/Scripts/Control/AIController.cs
@@ -8,31 +8,58 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspicionTime = 5f;
         private GameObject player;
         private Fighter fighter;
         private Health health;
         private Mover mover;
+        private ActionScheduler actionScheduler;
 
         private Vector3 guardPosition;
+        private float timeSinceLastSawPlayer = Mathf.Infinity;
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             player = GameObject.FindGameObjectWithTag("Player");
             health = GetComponent<Health>();
             mover = GetComponent<Mover>();
+            actionScheduler = GetComponent<ActionScheduler>();
             guardPosition = transform.position;
         }
         private void Update()
         {
             if (health.IsDead()) return;
-            if (InAttackRange() && fighter.CanAttack(player)) {
-                fighter.Attack(player);
+            if (InAttackRange() && fighter.CanAttack(player))
+            {
+                timeSinceLastSawPlayer = 0f;
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer<suspicionTime)
+            {
+                SuspictionBehaviour();
             }
             else
             {
-                mover.StartMoveAction(guardPosition);
+                GuardBehaviour();
             }
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
+        }
+
+        private void SuspictionBehaviour()
+        {
+            actionScheduler.CancelCurrentAction();
+        }
+
         private bool InAttackRange()
         {
             float distanceToPlayer= Vector3.Distance(player.transform.position, transform.position);
---
+++ RPG Project/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        [SerializeField] private float sizeOfWaypointGizmos=5f;
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            for (int i=0;i<transform.childCount;i++)
+            {
+                Transform childTransform = transform.GetChild(i);
+                Gizmos.DrawSphere(childTransform.position, sizeOfWaypointGizmos);
+            }
+        }
+    }
+}
