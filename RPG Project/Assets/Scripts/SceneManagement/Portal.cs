7c4e0da4de07c94fa46a3e46cacc3f890bd05570
Ikuti <m.j.olkiewicz@gmail.com>
2020-11-05 00:48:48 +0100

    Changed portal script to spawn in a specific spawn point in new scene

--- RPG Project/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG Project/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,19 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.AI;
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
         [SerializeField] private int sceneToLoadId = -1;
+        [SerializeField] private Transform spawnPoint;
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoadId);
+                StartCoroutine(Transition());
             }
+        }
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoadId);
+
+            Portal otherPortal = GetOtherPortal();
+
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(var portal in FindObjectsOfType<Portal>())
+            {
+                if (portal != this)
+                {
+                    return portal;
+                }
+            }
+            return null;
         }
     }
 }
