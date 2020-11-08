96934d1281968b05ea1e1cbf6af937316cf9667c
Ikuti <m.j.olkiewicz@gmail.com>
2020-11-08 23:41:42 +0100

    Created saving wrapper

---
+++ RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,40 @@
+﻿using RPG.Saving;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+        private SavingSystem savingSystem;
+        private void Start()
+        {
+            savingSystem = GetComponent<SavingSystem>();
+        }
+        private void Update()
+        {
+            if (Keyboard.current.lKey.wasPressedThisFrame)
+            {
+                Load();
+            }
+            if (Keyboard.current.sKey.wasPressedThisFrame)
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            savingSystem.Save(defaultSaveFile);
+        }
+
+        private void Load()
+        {
+            savingSystem.Load(defaultSaveFile);
+        }
+    }
+}
