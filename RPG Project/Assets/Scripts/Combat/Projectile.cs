559224f289e15e0fa245408bc5a2378f12ca3765
Ikuti <m.j.olkiewicz@gmail.com>
2021-02-05 23:29:29 +0100

    Changed projectile to allow for homing and not homing types

--- RPG Project/Assets/Scripts/Combat/Projectile.cs
+++ RPG Project/Assets/Scripts/Combat/Projectile.cs
@@ -8,16 +8,20 @@ public class Projectile : MonoBehaviour
 {
     
     [SerializeField] private float projectileSpeed = 1f;
+    [SerializeField] private bool isHoming=false;
 
     private Health target = null;
 
     float damage = 0;
-
+    private void Start()
+    {
+        transform.LookAt(GetAimLocation());
+    }
     // Update is called once per frame
     void Update()
     {
         if (target == null) return;
-        transform.LookAt(GetAimLocation());
+        if(isHoming && !target.IsDead()) transform.LookAt(GetAimLocation());
         transform.Translate(Vector3.forward*Time.deltaTime*projectileSpeed);
     }
 
@@ -25,6 +29,7 @@ public class Projectile : MonoBehaviour
     {
         this.target = target;
         this.damage = damage;
+        
     }
 
     private Vector3 GetAimLocation()
@@ -39,6 +44,7 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Health>() != target) return;
+        if (target.IsDead()) return;
         target.TakeDamage(damage);
         Destroy(gameObject);
     }
